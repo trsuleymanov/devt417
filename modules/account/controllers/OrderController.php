406b8cb73dd50885acd75d6b0959bc0a317690c5
vlad <vlad.shetinin@gmail.com>
2019-12-24 17:17:43 +0300

    - сделал чтобы на странице Активных заказов отображались новые заказы созданные в crm (со статусами: 'created_with_time_confirm', 'created_without_time_confirm', 'created_with_time_sat')

--- modules/account/controllers/OrderController.php
+++ modules/account/controllers/OrderController.php
@@ -31,7 +31,13 @@ class OrderController extends Controller
         $orders = ClientExt::find()
             ->where(['user_id' => Yii::$app->getUser()->getId()])
             //->andWhere(['!=', 'status', ''])
-            ->andWhere(['but_checkout' => ['reservation', '']])
+            ->andWhere([
+                'OR',
+                ['but_checkout' => 'reservation'],
+                ['but_checkout' => ''],
+                ['but_checkout' => NULL],
+            ])
+
             ->andWhere(['status' => ['created_with_time_confirm', 'created_without_time_confirm', 'created_with_time_sat']]) // pending_send - не беру, 'sended' - не беру
             ->orderBy(['id' => SORT_DESC])
             ->all();
