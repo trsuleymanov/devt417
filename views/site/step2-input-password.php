cf6f27e0ead613bdd968c16c20ce9e8d6d75bed0
vlad <vlad.shetinin@gmail.com>
2020-01-06 20:33:24 +0300

    - для десктопного /views/site/step2-input-password.php отключил загрузку jquery

--- views/site/step2-input-password.php
+++ views/site/step2-input-password.php
@@ -8,6 +8,8 @@ $this->title = 'Вход на сайт';
 $this->params['breadcrumbs'][] = $this->title;
 
 //$this->registerJsFile('js/login.js', ['depends'=>'app\assets\AppAsset']);
+Yii::$app->assetManager->bundles['yii\web\JqueryAsset'] = false;
+
 ?>
 <div class="site-login">
 
